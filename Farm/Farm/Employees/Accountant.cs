eda674f29617e4d33073552a49a129a44ca1806c
Yegor <ef188209@gmail.com>
2025-10-12 12:10:16 +0300

    chore: import Farm and update .gitignore

---
+++ Farm/Farm/Employees/Accountant.cs
@@ -0,0 +1,52 @@
+using Farm.Configs;
+using Farm.Interfaces;
+namespace Farm.Employees;
+
+public class Accountant(IEnumerable<IWorker> employees, EmployeeConfig? config = null)
+    : Employee(config ?? DefaultConfig)
+{
+    private static readonly EmployeeConfig DefaultConfig = new()
+    {
+        Name = "Accountant",
+        Age = 30,
+        Level = EmployeeLevel.Middle
+    };
+
+    private readonly EmployeeConfig _config = config ?? DefaultConfig;
+
+    private readonly SalaryConfig _salaryConfig = new();
+
+    public override void Work()
+    {
+        foreach (var employee in employees) PayEmployee(employee);
+
+        PayEmployee(this);
+    }
+
+    private void PayEmployee(IWorker employee)
+    {
+        var rate = _salaryConfig.GetRate(employee);
+        var salary = rate * employee.GetWorkCount();
+        if (salary < SalaryConfig.GetMinSalary())
+            salary = SalaryConfig.GetMinSalary();
+
+        employee.ReceiveSalary(salary);
+        PromoteIfNeeded(employee);
+
+        employee.ResetWorkCount();
+        Console.WriteLine($"Начислено {salary} {employee.GetName()} ({employee.GetType().Name})");
+    }
+
+    private void PromoteIfNeeded(IWorker employee)
+    {
+        var thresholds = _salaryConfig.GetPromotionThresholds(employee);
+        var currentLevel = employee.GetLevel();
+        if ((int)currentLevel < thresholds.Count && employee.GetWorkCount() >= thresholds[(int)currentLevel])
+            employee.SetLevel((EmployeeLevel)((int)currentLevel + 1));
+    }
+
+    public override void StopWork()
+    {
+        Console.WriteLine($"{_config.Name} завершил расчёт зарплат.");
+    }
+}
